c369b07e2695331e4a417c16e409b5ae735b3d2b
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-07-02 20:58:03 -0500

    try to fix image placement

---
+++ effects.js
@@ -0,0 +1,7 @@
+var accordion = document.getElementsByClassName("accordion");//get each accordion component
+
+var i;//iterator to go through each accordion
+
+for(i = 0;i< accordion.length;i++){
+
+}
