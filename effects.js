a3b22f4997cbe2858140809e53d7f0cebe7e1b3c
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-07-18 11:36:17 -0500

    tried adding some shit

--- effects.js
+++ effects.js
@@ -3,5 +3,21 @@ var accordion = document.getElementsByClassName("accordion");//get each accordio
 var i;//iterator to go through each accordion
 
 for(i = 0;i< accordion.length;i++){
+    accordion[i].addEventListener("click",function(){
+        /*
+            toggle between adding and removing "active class".
+            toggle helps hilight button that controls the panel.
+         */
 
+         this.classList.toggle("active");
+
+         /*toggle between hiding and showing active panel: */
+         var panel = this.nextElementSibling;
+         if(panel.style.display === "block"){
+            panel.style.display = "none";
+         }
+         else{
+             panel.style.display = "block";
+         }
+    });
 }
